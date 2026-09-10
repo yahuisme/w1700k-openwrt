afd7ba04d7c90fa4102364547ae4b12b6d6700f7
yahuisme <40222380+yahuisme@users.noreply.github.com>
2026-09-11 01:23:00 +0800

    chore: remove firmware updater

--- user/default/custom.sh
+++ user/default/custom.sh
@@ -19,8 +19,7 @@ if ! git clone --depth=1 https://github.com/yahuisme/packages.git "$PKG_REPO"; t
 fi
 cp -r "$PKG_REPO/luci-app-wifi7" "$PKG_REPO/luci-app-airoha-npu" \
       "$PKG_REPO/luci-app-airoha-flowsense" \
-      "$PKG_REPO/luci-app-airoha-fancontrol" \
-      "$PKG_REPO/luci-app-firmwareupgrade" package/
+      "$PKG_REPO/luci-app-airoha-fancontrol" package/
 
 # -------------------------------------------------
 # Existing W1700K custom files
